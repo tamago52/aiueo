353813c47ed105433144f6665d377fc6f76053d6
tamago52 <s2111292@s.tsukuba.ac.jp>
2023-09-27 20:20:46 +0900

    自由にわかりやすいメッセージ

--- db/schema.rb
+++ db/schema.rb
@@ -30,15 +30,6 @@ ActiveRecord::Schema.define(version: 2023_08_23_081240) do
     t.index ["tag_id"], name: "index_post_tag_relations_on_tag_id"
   end
 
-  create_table "post_tags", force: :cascade do |t|
-    t.integer "register_id", null: false
-    t.integer "tag_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["register_id"], name: "index_post_tags_on_register_id"
-    t.index ["tag_id"], name: "index_post_tags_on_tag_id"
-  end
-
   create_table "posts", force: :cascade do |t|
     t.string "image"
     t.string "place"
@@ -74,6 +65,4 @@ ActiveRecord::Schema.define(version: 2023_08_23_081240) do
   add_foreign_key "likes", "users"
   add_foreign_key "post_tag_relations", "posts"
   add_foreign_key "post_tag_relations", "tags"
-  add_foreign_key "post_tags", "registers"
-  add_foreign_key "post_tags", "tags"
 end
